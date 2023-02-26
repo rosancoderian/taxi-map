b7b84b15043b415095fc5d4784aba9cf5b7f2bf9
rosancoderian <rosancoderian@gmail.com>
2023-02-26 21:40:54 +0700

    feat: add Carbon component library

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,21 @@
 <script>
-	import Map from '../lib/components/Map/Map.svelte';
+	import Map from '$lib/components/Map/Map.svelte';
+	import { DatePicker, DatePickerInput } from 'carbon-components-svelte';
+	let selectedDate = '';
 </script>
 
 <Map />
+<div class="mapControl">
+	<DatePicker bind:value={selectedDate} dateFormat="Y-m-d" datePickerType="single">
+		<DatePickerInput placeholder="yyyy-mm-dd" />
+	</DatePicker>
+</div>
+
+<style>
+	.mapControl {
+		position: absolute;
+		top: 20px;
+		left: 100px;
+		z-index: 400;
+	}
+</style>
