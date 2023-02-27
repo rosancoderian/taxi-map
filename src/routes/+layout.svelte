5a7f205946d39127a1151b0cd529def36fe1b05a
rosancoderian <rosancoderian@gmail.com>
2023-02-27 09:23:19 +0700

    feat: add nprogress

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,18 @@
 <script>
+	import NProgress from 'nprogress'
+	import { navigating } from '$app/stores'
+	import 'nprogress/nprogress.css'
 	import '../app.postcss'
 	import 'carbon-components-svelte/css/white.css'
+
+	NProgress.configure({
+		minimum: 0.16
+	})
+
+	$: {
+		if ($navigating) NProgress.start()
+		if (!$navigating) NProgress.done()
+	}
 </script>
 
 <slot />
